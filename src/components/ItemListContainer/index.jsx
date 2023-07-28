00ba37df5d58f4e45c0144ade7cd8bd920c501ae
Bruno Calace <bruno.calace@gmail.com>
2023-07-28 17:40:12 -0300

    Lo anterior con setTimeout

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -9,11 +9,14 @@ function ItemListContainer() {
     useEffect(() => {
         fetch('/src/mocks/data.json')
           .then((response) => response.json())
-          .then((data) => setProducts(data))
+          .then((data) => {
+            setTimeout(() => {
+                setProducts(data)
+                }, 2000)
+            })
+
           .catch((error) => console.error('Error fetching data:', error));
       }, []);
-
-    console.log(products);
 
     return (
         <div className="item-list-container">
